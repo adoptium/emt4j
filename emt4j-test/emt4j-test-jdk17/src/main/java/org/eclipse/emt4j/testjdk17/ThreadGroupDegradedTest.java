84abd8b213584face710ad3949a970af92978345
yifeng.jyf <yifeng.jyf@alibaba-inc.com>
2023-12-19 11:41:33 +0800

    rule thread group degraded

---
+++ emt4j-test/emt4j-test-jdk17/src/main/java/org/eclipse/emt4j/testjdk17/ThreadGroupDegradedTest.java
@@ -0,0 +1,37 @@
+/********************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+package org.eclipse.emt4j.testjdk17;
+
+import org.eclipse.emt4j.common.JsonReport;
+import org.eclipse.emt4j.test.common.SITBaseCase;
+import org.eclipse.emt4j.test.common.TestConf;
+
+
+@TestConf(mode = {TestConf.ModeEnum.AGENT, TestConf.ModeEnum.CLASS}, from = TestConf.RELEASE.JDK17, to = TestConf.RELEASE.JDK21)
+public class ThreadGroupDegradedTest extends SITBaseCase {
+    public void run() {
+        ThreadGroup threadGroup = new ThreadGroup("test");
+        threadGroup.destroy();
+    }
+
+    @Override
+    public void verify(JsonReport jsonReport) {
+        assertTrue(matchAny(jsonReport, "THREADGROUP_DEGRADED"));
+    }
+}
